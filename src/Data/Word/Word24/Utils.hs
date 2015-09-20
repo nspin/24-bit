10e1ab3d7370f271f6a788067f9f8f0ab0aa2704
Nick Spinale <spinalen@carleton.edu>
2015-09-20 18:01:23 -0500

    fized error in utils

--- src/Data/Word/Word24/Utils.hs
+++ src/Data/Word/Word24/Utils.hs
@@ -3,14 +3,17 @@ module Data.Word.Word24.Utils
     , toOctets
     ) where
 
-import Data.Word.Word24
 import Data.Bits
+import Data.Word
+import Data.Word.Word24
 
 fromOctets :: Word8 -> Word8 -> Word8 -> Word24
-fromOctets a b c = a .|. shift 8 b .|. shift 16 c
+fromOctets a b c =  fromIntegral a
+                .|. (fromIntegral b `shiftR` 8)
+                .|. (fromIntegral c `shiftR` 16)
 
 toOctets :: Word24 -> (Word8, Word8, Word8)
 toOctets w = ( fromIntegral w
-             , fromIntegral (shift -8 w)
-             , fromIntegral (shift -16 w)
+             , fromIntegral (w `shiftL` 8)
+             , fromIntegral (w `shiftL` 16)
              )
